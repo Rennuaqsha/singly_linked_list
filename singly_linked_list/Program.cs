c2f3ec28a44f99df0f433520d99557133faca42f
Rennu Aqsha <rennu.aqsha.ft21@mail.umy.ac.id>
2022-11-07 18:02:34 +0800

    create public list and public void addnote

--- singly_linked_list/Program.cs
+++ singly_linked_list/Program.cs
@@ -5,7 +5,7 @@ namespace singly_linked_list
     //each node consist of the information part and link to the next node
     class node
     {
-        public int rollNumber:
+        public int rollNumber;
         public string name;
         public node next;
     }
@@ -21,12 +21,31 @@ namespace singly_linked_list
 
         public void addNote()
         {
-            
+            int rollNo;
+            string nm;
+            Console.Write("\n Enter the roll number of the student: ");
+            rollNo = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n Enter the roll number of the student: ");
+            nm = Console.ReadLine();
+            node newnode = new node();
+            newnode.rollNumber = rollNo;
+            newnode.name = nm;
+            if(START != null || (rollNo == START.rollNumber))
+            {
+                if ((START != null) &&(rollNo == START.rollNumber))
+                {
+                    Console.WriteLine();
+                    return;
+                }
+                newnode.next = START;
+                START = newnode;
+                return;
+            }
         }
     }
 
     class program
     {
-        static void ma
+        
     }
 }

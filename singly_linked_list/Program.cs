97c6f2a5c4f374fc5457154ed0b291c66ff50877
Rennu Aqsha <rennu.aqsha.ft21@mail.umy.ac.id>
2022-11-07 18:43:32 +0800

    create menu in class program

--- singly_linked_list/Program.cs
+++ singly_linked_list/Program.cs
@@ -113,6 +113,23 @@ namespace singly_linked_list
 
     class program
     {
-        
+        static void Main(String[] args)
+        {
+            list obj = new list();
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("\nMenu");
+                    Console.WriteLine("1. Add a record to the list");
+                    Console.WriteLine("2. Delete a record from the list");
+                    Console.WriteLine("3. view all the records in the list");
+                    Console.WriteLine("4. Search for a record in the list");
+                    Console.WriteLine("5. Exit");
+                    Console.Write("\n Enter your choice (1-5) : ");
+                    
+                }
+            }
+        }
     }
 }

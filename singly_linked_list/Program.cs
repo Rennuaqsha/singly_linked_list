fbe94c27e66f9835622b6d8312fe1ad4a598d1d5
Rennu Aqsha <rennu.aqsha.ft21@mail.umy.ac.id>
2022-11-07 17:55:26 +0800

    declare variables in the node class

--- singly_linked_list/Program.cs
+++ singly_linked_list/Program.cs
@@ -2,5 +2,31 @@
 
 namespace singly_linked_list
 {
+    //each node consist of the information part and link to the next node
+    class node
+    {
+        public int rollNumber:
+        public string name;
+        public node next;
+    }
 
+    class list
+    {
+        node START; 
+
+        public list()
+        {
+            START = null;
+        }
+
+        public void addNote()
+        {
+            
+        }
+    }
+
+    class program
+    {
+        static void ma
+    }
 }
